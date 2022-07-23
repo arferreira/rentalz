b6759122464eda319a0daf35daf2079b6dc2b4ec
Antonio Souza <antonio.eschola@gmail.com>
2022-07-23 17:16:30 -0400

    Create repositories to manage data on categories model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,18 @@
 import { Router } from "express";
 import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository
 
-const categories: Category[] = [];
+
 
 // Endpoint to create categories
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  });
-
-  categories.push(category);
-
-  return response.status(201).json({ categories });
+  categoriesRepository.create({ name, description });
+  return response.status(201).json({ message: 'Category was success created!' });
 });
 
 
