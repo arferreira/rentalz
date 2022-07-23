2c66dc32a66e94bbdb58e8601811e170abc4356e
Antonio Souza <antonio.eschola@gmail.com>
2022-07-23 16:55:06 -0400

    Isolate model to work with category data

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,21 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/Category";
 
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 // Endpoint to create categories
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4,
+  const category: Category = {
+    id: uuidV4(),
     name: name,
-    description: description
+    description: description,
+    created_at: new Date()
   };
 
   categories.push(category);
