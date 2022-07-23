90e0ec284381796f5cc087de724524e6747feceb
Antonio Souza <antonio.eschola@gmail.com>
2022-07-23 17:25:51 -0400

    validate create category with same name

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -11,6 +11,10 @@ const categoriesRepository = new CategoriesRepository
 // Endpoint to create categories
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if(categoryAlreadyExists){
+    return response.status(400).json({ error: 'Category already exists!' });  
+  }
   categoriesRepository.create({ name, description });
   return response.status(201).json({ message: 'Category was success created!' });
 });
